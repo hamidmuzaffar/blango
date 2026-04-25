debeee25e634ffc66e190da541e6dd526d046037
Hamid Khan <hkhan.bscs20seecs@seecs.edu.pk>
2026-04-25 07:18:24 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,7 @@ class Dev(Configuration):
       'blog',
       'blango_auth',
       "rest_framework",
+      "rest_framework.authtoken",
       "debug_toolbar",
       'crispy_forms',
       'crispy_bootstrap5',
@@ -194,6 +195,14 @@ class Dev(Configuration):
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
   ACCOUNT_ACTIVATION_DAYS = 7
+
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+  }
 
 class Prod(Dev):
   DEBUG = False
