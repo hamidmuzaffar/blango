ee395e7bf25258b1bd338f4303adef62d0b0db57
Hamid Khan <hkhan.bscs20seecs@seecs.edu.pk>
2026-05-21 05:49:00 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,50 @@
-const theNumber = 1
-let yourName = 'Ben'
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
 
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
 }
 
-alert(yourName)
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
+
+
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
+
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
